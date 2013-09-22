c4190ff6b2feaf0f2c678ac6c62a88e965634754
Rowan <rowantyj@gmail.com>
2013-09-22 21:16:30 +0800

    Reminder class & layout

---
+++ src/com/madmonkey/magnifitnessClass/Reminder.java
@@ -0,0 +1,15 @@
+package com.madmonkey.magnifitnessClass;
+
+import java.util.*;
+
+public class Reminder {
+	
+	private String meal;
+	private Date date;
+	private boolean status;
+
+	public Reminder() {
+		
+	}
+
+}
