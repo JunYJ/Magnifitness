0fa36124507da334b83a43ad5b1b8427d228dda6
JunYJ <jason_arsenal20@yahoo.com>
2013-11-04 19:40:46 +0800

    Reminder

--- src/com/madmonkey/magnifitnessClass/Reminder.java
+++ src/com/madmonkey/magnifitnessClass/Reminder.java
@@ -8,8 +8,41 @@ public class Reminder {
 	private Date date;
 	private boolean status;
 
-	public Reminder() {
-		
+	public Reminder() 
+	{
+		meal = "";
+		date = null;
+		status = false;
+	}
+	
+	public String getMeal()
+	{
+		return meal;
+	}
+	
+	public Date getDate()
+	{
+		return date;
+	}
+	
+	public boolean getStatus()
+	{
+		return status;
+	}
+	
+	public void setMeal(String mealIn)
+	{
+		meal = mealIn;
+	}
+	
+	public void setDate(Date dateIn)
+	{
+		date = dateIn;
+	}
+	
+	public void setStatus(boolean statusIn)
+	{
+		status = statusIn;
 	}
 
 }
