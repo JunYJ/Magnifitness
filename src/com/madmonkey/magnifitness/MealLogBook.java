68dae05c26ef1dc6dfb14e29e9470f2c5c48054e
Rowan <rowantyj@gmail.com>
2014-01-17 14:12:02 +0800

    syncing together first

--- src/com/madmonkey/magnifitness/MealLogBook.java
+++ src/com/madmonkey/magnifitness/MealLogBook.java
@@ -5,22 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.ListView;
 
 public class MealLogBook extends Fragment {
 
+	ListView mealList;
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+		{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-	}
+		}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.meal_logbook, container,
-				false);
-
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+		View rootView = inflater.inflate(R.layout.meal_logbook, container, false);
+		mealList = (ListView) rootView.findViewById(R.id.mealList);
+		
+		
+		
 		return rootView;
-	}
+		}
 
 }
