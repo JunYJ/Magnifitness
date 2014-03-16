f7650dd6b7d50465f12db774006d419d01618d26
Rowan <rowantyj@gmail.com>
2014-03-16 17:10:00 +0800

    mealEntry need to load foodlist

--- src/com/madmonkey/magnifitness/MealEntry.java
+++ src/com/madmonkey/magnifitness/MealEntry.java
@@ -1,11 +1,12 @@
 package com.madmonkey.magnifitness;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -13,10 +14,14 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.madmonkey.magnifitness.util.DatabaseHandler;
+import com.madmonkey.magnifitnessClass.Food;
+
 public class MealEntry extends Activity implements OnClickListener {
 
 	Button				add, confirm;
 	String[]			foodList;
+	DatabaseHandler dbHandler;
 
 	final static int	BREAKFAST	= 1;
 	final static int	LUNCH		= 2;
@@ -59,7 +64,6 @@ public class MealEntry extends Activity implements OnClickListener {
 		add.setOnClickListener(this);
 		confirm.setOnClickListener(this);
 
-		foodList = getResources().getStringArray(R.array.food_list);
 		}
 
 	@Override
@@ -90,8 +94,9 @@ public class MealEntry extends Activity implements OnClickListener {
 
 							}
 					});
+
 				searchDialog.show();
-				
+				onSearchRequested();
 				break;
 
 			case R.id.confirm_btn:
